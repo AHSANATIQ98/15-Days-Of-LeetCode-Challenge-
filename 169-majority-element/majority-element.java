ab6726d4271642a449fdd0cffa952f25b365aeea
Muhammad Ahsan Atiq <SP24-BDS-028@ISBSTUDENT.COMSATS.EDU.PK>
2025-02-04 19:18:04 +0500

    Time: 2 ms (75.44%) | Memory: 53.1 MB (58.97%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,28 +1,19 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        int prevkami = 0;  // To store the majority element
-        int prevkamiCount = 0;  // To store the count of majority element
-        
+        int freq = 0;  
+        int n = 0;
         for (int i = 0; i < nums.length; i++) {
-            int kami = 0;  // Reset for each element in the outer loop
-            
-            for (int j = 0; j < nums.length; j++) {
-                if (nums[i] == nums[j]) {
-                    kami++;  // Count the occurrences of nums[i]
-                }
+            if (freq == 0){
+                n = nums[i]; 
+            }
+            if (nums[i] == n){
+                freq++;
+            } 
+            if (nums[i] != n){
+                freq--;
             }
             
-            if (kami > prevkamiCount) {
-                prevkamiCount = kami;  // Update the count if the current element is more frequent
-                prevkami = nums[i];  // Update the majority element
-            }
-            
-            // If we've found an element whose count exceeds half of the length, return it
-            if (prevkamiCount > nums.length / 2) {
-                return prevkami;
-            }
-        }
-        
-        return prevkami;  // Return the majority element (though by problem statement, it should always be found)
+        } 
+        return n;
     }
 }
