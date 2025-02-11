7674b0dabdbc642ffb03273cde3150ada412bba1
Muhammad Ahsan Atiq <SP24-BDS-028@ISBSTUDENT.COMSATS.EDU.PK>
2025-02-11 22:07:44 +0500

    Time: 0 ms (100.00%) | Memory: 63.6 MB (12.97%) - LeetSync

--- 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.cpp
+++ 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.cpp
@@ -1,13 +1,21 @@
 class Solution {
 public:
     int peakIndexInMountainArray(vector<int>& arr) {
-        int i;
-        for (i = 0; i<arr.size()-2; i++){
-            if (arr[i] > arr[i+1]){
-                break;
+        int i = 1;
+        int j = arr.size()-2;
+        int m;
+        while (i <= j){
+            m = i + (j-i)/2;
+
+            if (arr[m]> arr[m-1] && arr[m] > arr[m+1]){
+                return m;
+            } else if (arr[m] > arr[m-1]){
+                i = m+1;
+            } else {
+                j = m-1;
             }
         }
-        return i;
+        return -1;
         
     }
 };
